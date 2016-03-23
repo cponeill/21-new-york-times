e664272f1db764710cd695734c25c63bea7ddd9b
Casey O'Neill <caseyoneill78@hotmail.com>
2016-03-23 09:11:54 -0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,33 @@
+# Loading developer libraries
+
+import requests
+import os
+import urllib
+from flask import Flask, request
+from two1.lib.wallet import Wallet
+from two1.lib.bitserv.flask import Payment
+
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+# go to http://developer.nytimes.com/ and choose the top-stories API key.
+api_key = "<add api-key>"
+
+# Adding 402 end-point
+@app.route('/top-stories', methods=['GET', 'POST'])
+@payment.required(10)
+def top_stories():
+     """
+     Choose the section of the NYTimes you would like to read
+     """
+     section = request.form['section']
+
+     url = requests.get('http://api.nytimes.com/svc/topstories/v1/'+section+'.j\
+son?api-key='+api_key)
+
+     return url.text
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', debug=True)
